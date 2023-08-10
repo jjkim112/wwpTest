c680ca5fb4b19f78369781a67f906c5589ad3bf2
NangmanCo <admin@nangman.io>
2023-08-10 21:18:45 +0900

    통합

--- src/page/holdem_pub/pub_page/HoldemPubOnePage.tsx
+++ src/page/holdem_pub/pub_page/HoldemPubOnePage.tsx
@@ -1,5 +1,12 @@
+import { Pub } from "../../../domain/Pub.model";
+import {useState} from 'react';
+
 export function HoldemPubOnePage(pubId: string) {
+    const [thisPub, setThisPub] = useState<Pub | null>(null);
   return (
+    thisPub === null ? 
+    <div>로딩중 화면</div>
+    :
     <div>
       holdem pub one page
       <div>ID : {pubId}</div>
